5b45d94592ec2841cb9c1d41f8723b234caf0636
godhwanipriya <priyagodhwani123@gmail.com>
2024-07-09 18:13:00 +0530

    completed_task-2

---
+++ emp_data.sql
@@ -0,0 +1,50 @@
+create table emp_data (
+	id serial primary key,
+	name varchar(150) not null,
+	age int,
+	gender varchar,
+	department varchar,
+	city varchar,
+	experience varchar,
+	salary int
+	)
+
+	select * from emp_data
+
+insert into emp_data values (01,'priya',26,'F','IT','Nagpur',1,30000)
+insert into emp_data values (02,'rohit',29,'M','sales','nagpur',5,100000)
+insert into emp_data values (03,'ruch',35,'F','Human Resources','mumbai',2,35000)
+insert into emp_data values (04,'anil',40,'M','Engineering','pune',10,150000)
+insert into emp_data values (05,'piyush',40,'M','Finance','jaipur',1,25000)
+insert into emp_data (id,name,age,gender,department,city,experience,salary)
+    values 
+	(06,'nikita',26,'F','Finance','benglore',1,25000),
+insert into emp_data values (07,'manya',35,'F','marketing','surat',3,35000)
+  insert into emp_data values (08,'payal',45,'F','accounting','bhopal',5,55000),
+    (09,'shrichand',60,'M','education','mumbai',20,250000),
+    (10,'kapil',30,'M','sales','pune',1,20000)
+insert into emp_data 
+	values
+(31,'gudiya',30,'f','teacher','mumbai',1,20000),
+(32,'kunal',25,'m','sales','pune',2,50000),
+(33,'ishu',27,'m','finance','benglore',1,25000),
+(34,'rinki',45,'f','marketing','amrawati',2,30000),
+(35,'sweety',30,'f','admin','nagpur',1,25000),
+(36,'simran',34,'f','it','jaipur',3,35000),
+(37,'gaurav',20,'m','it','pune',1,20000),
+(38,'bhumi',25,'f','sales','amrawati',1,25000),
+(39,'kanchan',36,'f','admin','bhopal',6,45000),
+(40,'ruchi',40,'m','sales','nagpur',4,35000)
+insert into emp_data 
+	values
+(41,'sarika',28,'f','marketing','jodhpur',2,60000),
+(42,'abhijeet',30,'m','finance','pune',1,25000),
+(43,'apurva',27,'f','admin','nagpur',2,30000),
+(44,'neha',35,'f','sales','amritsar',1,20000),
+(45,'chanu',20,'F','marketing','mumbai',10,500000),
+(46,'sarita',36,'f','it','benglore',20,500000),
+(47,'rounak',25,'m','accounting','mumbai',4,40000),
+(48,'chanda',55,'f','sales','pune',25,1000000),
+(49,'ramesh',50,'m','marketing','jaipur',10,100000),
+(50,'rupesh',45,'m','sales','mumbai',1,30000)
+
